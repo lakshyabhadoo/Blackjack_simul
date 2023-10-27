866e96d84baf6af490e8adcfe3eb080ebd9f4eb7
Lakshya_laptop <lakshya.bhadoo@gmail.com>
2023-10-27 11:36:33 +0530

    testing

--- blackjack.py
+++ blackjack.py
@@ -5,7 +5,7 @@ import random
 def load_images(card_images):
     suits = ['heart', 'club', 'diamond', 'spade']
     face_cards = ['jack', 'queen', 'king']
-    
+    print("Load Images") 
     if tkinter.TkVersion >= 8.6:
         extension = 'png'
     else:
